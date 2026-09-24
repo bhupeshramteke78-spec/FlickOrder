986c2ac700edd0d8bc76d155bf691091b3b00424
bhupeshramteke78-spec <bhupeshramteke78@gmail.com>
2026-09-24 21:19:21 +0530

    fix: configure remotePatterns for unsplash images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   allowedDevOrigins: ["192.168.1.33"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
   turbopack: {
     root: process.cwd(),
   },
